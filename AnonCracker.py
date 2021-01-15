40e65f7e52ecbe369e815956258af443280b1cea
Subodha Prabash <73874882+thelinuxuser-choice@users.noreply.github.com>
2021-01-15 13:23:14 +0530

    Update AnonCracker.py

--- AnonCracker.py
+++ AnonCracker.py
@@ -7,7 +7,7 @@ bold='\033[01m'
 TGREEN = '\033[92m' # Green Text
 WARNING = '\033[93m' #nothing
 FAIL = '\033[91m' #just shut up
-UNDERLINE = '\033[4m' #burp with suite
+
 
 print(WARNING+bold+"   ('-.         .-') _                   .-') _             _   .-')                               .-') ")
 print(WARNING+bold+"  ( OO ).-.    ( OO ) )                 ( OO ) )           ( '.( OO )_                            ( OO ).")
@@ -23,7 +23,7 @@ print(FAIL+" =    THIS IS A SCRET PYTHON SCRIPT TO   DEVELOP FREEKING HACKERS AB
 print(FAIL+" =          CODED BY PRABASH                                                                              =")
 print(FAIL+" =    IF DID THIS HELP FOR YOU YOU CAN SIMPLY BY ME A COFFEE OR FOLLOW ME ON INSTA:=h3k3rs                =")
 print(FAIL+" ==========================================================================================================")
-print(TGREEN+UNDERLINE+"--------------THIS IS FOR EDUCATIONAL PURPOSE ONLY-I ALREADY WARNED YOU!---------------")
+print(TGREEN+"--------------THIS IS FOR EDUCATIONAL PURPOSE ONLY-I ALREADY WARNED YOU!---------------")
 print(TGREEN +"                     ッ|simple bruteforce tool for     |ッ    ")
 print(TGREEN +"                      ☺|pdf,hash,zip files superfast   | ☺   ")
 print(TGREEN +"              ☠   ☠   ☠|donot use for illeagal purposes|☠  ☠  ☠  ")
