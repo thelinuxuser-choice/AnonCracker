549c5993ca5c44f6cc15a24bb25721cca89175ca
Subodha Prabash <73874882+thelinuxuser-choice@users.noreply.github.com>
2021-01-15 13:11:31 +0530

    added new banner

--- brutex.py
+++ brutex.py
@@ -1,19 +1,29 @@
 TGREEN =  '\033[92m' # Green Text
 
 print(TGREEN +"    YOU DONOT BECOME A CODER BY JUST COPYING OTHER ONES CODES AND GET CREDITS TO YOU!   ")
-print(TGREEN +"─────────────────────────────────────────────────────────────────────────────────────────────────────")
-print(TGREEN +"─██████████████───████████████████───██████──██████─██████████████─██████████████─████████──████████─")
-print(TGREEN +"─██░░░░░░░░░░██───██░░░░░░░░░░░░██───██░░██──██░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░██──██░░░░██─")
-print(TGREEN +"─██░░██████░░██───██░░████████░░██───██░░██──██░░██─██████░░██████─██████████░░██─████░░██──██░░████─")
-print(TGREEN +"─██░░██──██░░██───██░░██────██░░██───██░░██──██░░██─────██░░██─────────────██░░██───██░░░░██░░░░██───")
-print(TGREEN +"─██░░██████░░████─██░░████████░░██───██░░██──██░░██─────██░░██─────██████████░░██───████░░░░░░████───")
-print(TGREEN +"─██░░░░░░░░░░░░██─██░░░░░░░░░░░░██───██░░██──██░░██─────██░░██─────██░░░░░░░░░░██─────██░░░░░░██─────")
-print(TGREEN +"─██░░████████░░██─██░░██████░░████───██░░██──██░░██─────██░░██─────██████████░░██───████░░░░░░████───")
-print(TGREEN +"─██░░██────██░░██─██░░██──██░░██─────██░░██──██░░██─────██░░██─────────────██░░██───██░░░░██░░░░██───")
-print(TGREEN +"─██░░████████░░██─██░░██──██░░██████─██░░██████░░██─────██░░██─────██████████░░██─████░░██──██░░████─")
-print(TGREEN +"─██░░░░░░░░░░░░██─██░░██──██░░░░░░██─██░░░░░░░░░░██─────██░░██─────██░░░░░░░░░░██─██░░░░██──██░░░░██─")
-print(TGREEN +"─████████████████─██████──██████████─██████████████─────██████─────██████████████─████████──████████─")
-print(TGREEN +"─────────────────────────────────────────────────────────────────────────────────────────────────────")
+# THIS IS A PYTHON  CODE TO DEVELOP YOUR BRAIN WITHOUT MUSCLES
+# I FORGOT THIS IS A CASE SENSITIVE LANG AND I TYPED Print else writing print _XD lol!
+bold='\033[01m'
+TGREEN = '\033[92m' # Green Text
+WARNING = '\033[93m' #nothing
+FAIL = '\033[91m' #just shut up
+UNDERLINE = '\033[4m' #burp with suite
+
+print(WARNING+bold+"   ('-.         .-') _                   .-') _             _   .-')                               .-') ")
+print(WARNING+bold+"  ( OO ).-.    ( OO ) )                 ( OO ) )           ( '.( OO )_                            ( OO ).")
+print(WARNING+bold+"  / . --. /,--./ ,--,'  .-'),-----. ,--./ ,--,'  ,--.   ,--.,--.   ,--.).-'),-----.  ,--. ,--.   (_)---\_)")
+print(WARNING+bold+"  | \-.  \ |   \ |  |\ ( OO'  .-.  '|   \ |  |\   \  `.'  / |   `.'   |( OO'  .-.  ' |  | |  |   /    _ |")
+print(WARNING+bold+".-'-'  |  ||    \|  | )/   |  | |  ||    \|  | ).-')     /  |         |/   |  | |  | |  | | .-') \  :` `.")
+print(WARNING+bold+" \| |_.'  ||  .     |/ \_) |  |\|  ||  .     |/(OO  \   /   |  |'.'|  |\_) |  |\|  | |  |_|( OO ) '..`''.)")
+print(WARNING+bold+"  |  .-.  ||  |\    |    \ |  | |  ||  |\    |  |   /  /\_  |  |   |  |  \ |  | |  | |  | | `-' /.-._)   \ ")
+print(WARNING+bold+"  |  | |  ||  | \   |     `'  '-'  '|  | \   |  `-./  /.__) |  |   |  |   `'  '-'  '('  '-'(_.-' \       /")
+print(WARNING+bold+"  `--' `--'`--'  `--'       `-----' `--'  `--'    `--'      `--'   `--'     `-----'   `-----'     `-----'")
+print(FAIL+" ==========================================================================================================")
+print(FAIL+" =    THIS IS A SCRET PYTHON SCRIPT TO   DEVELOP FREEKING HACKERS ABILITY BY A SCRIPT KIDDOS              =")
+print(FAIL+" =          CODED BY PRABASH                                                                              =")
+print(FAIL+" =    IF DID THIS HELP FOR YOU YOU CAN SIMPLY BY ME A COFFEE OR FOLLOW ME ON INSTA:=h3k3rs                =")
+print(FAIL+" ==========================================================================================================")
+print(TGREEN+UNDERLINE+"--------------THIS IS FOR EDUCATIONAL PURPOSE ONLY-I ALREADY WARNED YOU!---------------")
 print(TGREEN +"                     ッ|simple bruteforce tool for     |ッ    ")
 print(TGREEN +"                      ☺|pdf,hash,zip files superfast   | ☺   ")
 print(TGREEN +"              ☠   ☠   ☠|donot use for illeagal purposes|☠  ☠  ☠  ")
@@ -21,7 +31,7 @@ print(TGREEN +"                    ----♛coming soon ssh bruteforcer♛------  
 print(TGREEN +"===================❌made with evil love❌====================================")
 print(TGREEN +"=  ❌This Script Was Made By taj_0023 (thenga) and thelinuxuser-choice ❌        =")
 print(TGREEN +"=  ❌[youtube coming soon - coming soon with full of love]❌                 = ")
-print(TGREEN +"=        ❌[instergram - taj0023,h3k3rs]❌                                   = ")
+print(TGREEN +"=        ❌[instergram - taj0023,thelinuxuser_choice]❌                                   = ")
 print(TGREEN +"=============================================================================") 
 print(" -Here is your operating system-                          ")
 import os
