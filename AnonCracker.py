9ed5f853f352d0d9721248e3696cce82867615cc
Subodha Prabash <73874882+thelinuxuser-choice@users.noreply.github.com>
2021-03-17 18:28:00 +0530

    V 2.0

--- AnonCracker.py
+++ AnonCracker.py
@@ -7,46 +7,67 @@ bold='\033[01m'
 TGREEN = '\033[92m' # Green Text
 WARNING = '\033[93m' #nothing
 FAIL = '\033[91m' #just shut up
+print(FAIL +" ▄▄▄       ███▄    █  ▒█████   ███▄    █  ▄████▄   ██▀███   ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███   ")
+print(FAIL +"▒████▄     ██ ▀█   █ ▒██▒  ██▒ ██ ▀█   █ ▒██▀ ▀█  ▓██ ▒ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒ ")
+time.sleep(1)
+print(FAIL +"▒██  ▀█▄  ▓██  ▀█ ██▒▒██░  ██▒▓██  ▀█ ██▒▒▓█    ▄ ▓██ ░▄█ ▒▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒ ")
+print(FAIL +"░██▄▄▄▄██ ▓██▒  ▐▌██▒▒██   ██░▓██▒  ▐▌██▒▒▓▓▄ ▄██▒▒██▀▀█▄  ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄   ")
+print(FAIL +" ▓█   ▓██▒▒██░   ▓██░░ ████▓▒░▒██░   ▓██░▒ ▓███▀ ░░██▓ ▒██▒ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒ ")
+time.sleep(1)
+print(FAIL +" ▒▒   ▓▒█░░ ▒░   ▒ ▒ ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ░ ░▒ ▒  ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░ ")
+print(FAIL +"  ▒   ▒▒ ░░ ░░   ░ ▒░  ░ ▒ ▒░ ░ ░░   ░ ▒░  ░  ▒     ░▒ ░ ▒░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░ ")
+time.sleep(1)
+print(FAIL +"  ░   ▒      ░   ░ ░ ░ ░ ░ ▒     ░   ░ ░ ░          ░░   ░   ░   ▒   ░        ░ ░░ ░    ░     ░░   ░  ")
+print(FAIL +"      ░  ░         ░     ░ ░           ░ ░ ░         ░           ░  ░░ ░      ░  ░      ░  ░   ░  ")   
+print(FAIL +"                                         ░                           ░                              ")                                         ░                           ░                              ") 
 
+         
+print(TGREEN +"SELECT NO 1 FOR PDF BRUTE FORCE ")
+print("                             ")
+print(TGREEN +"SELECT NO 2 FOR ZIP BRUTE FORCE")
+print("                             ")
+print(TGREEN +"SELECT NO 3 FOR HASH BRUTEFORCE")
+print("                             ")
+print(TGREEN +"SELECT e    TO EXIT THE SCRIPT ")
+print("                             ")
 
-print(WARNING+bold+"   ('-.         .-') _                   .-') _             _   .-')                               .-') ")
-print(WARNING+bold+"  ( OO ).-.    ( OO ) )                 ( OO ) )           ( '.( OO )_                            ( OO ).")
-print(WARNING+bold+"  / . --. /,--./ ,--,'  .-'),-----. ,--./ ,--,'  ,--.   ,--.,--.   ,--.).-'),-----.  ,--. ,--.   (_)---\_)")
-print(WARNING+bold+"  | \-.  \ |   \ |  |\ ( OO'  .-.  '|   \ |  |\   \  `.'  / |   `.'   |( OO'  .-.  ' |  | |  |   /    _ |")
-print(WARNING+bold+".-'-'  |  ||    \|  | )/   |  | |  ||    \|  | ).-')     /  |         |/   |  | |  | |  | | .-') \  :` `.")
-print(WARNING+bold+" \| |_.'  ||  .     |/ \_) |  |\|  ||  .     |/(OO  \   /   |  |'.'|  |\_) |  |\|  | |  |_|( OO ) '..`''.)")
-print(WARNING+bold+"  |  .-.  ||  |\    |    \ |  | |  ||  |\    |  |   /  /\_  |  |   |  |  \ |  | |  | |  | | `-' /.-._)   \ ")
-print(WARNING+bold+"  |  | |  ||  | \   |     `'  '-'  '|  | \   |  `-./  /.__) |  |   |  |   `'  '-'  '('  '-'(_.-' \       /")
-print(WARNING+bold+"  `--' `--'`--'  `--'       `-----' `--'  `--'    `--'      `--'   `--'     `-----'   `-----'     `-----'")
-print(FAIL+" ==========================================================================================================")
-print(FAIL+" =    THIS IS A SCRET PYTHON SCRIPT TO   DEVELOP FREEKING HACKERS ABILITY BY A SCRIPT KIDDOS              =")
-print(FAIL+" =          CODED BY PRABASH                                                                              =")
-print(FAIL+" =    IF DID THIS HELP FOR YOU YOU CAN SIMPLY BY ME A COFFEE OR FOLLOW ME ON INSTA:=h3k3rs                =")
-print(FAIL+" ==========================================================================================================")
-print(TGREEN+"--------------THIS IS FOR EDUCATIONAL PURPOSE ONLY-I ALREADY WARNED YOU!---------------")
-print(TGREEN +"                     ッ|simple bruteforce tool for     |ッ    ")
-print(TGREEN +"                      ☺|pdf,hash,zip files superfast   | ☺   ")
-print(TGREEN +"              ☠   ☠   ☠|donot use for illeagal purposes|☠  ☠  ☠  ")
-print(TGREEN +"                    ----♛coming soon ssh bruteforcer♛------  \n")
-print(TGREEN +"===================❌made with evil love❌====================================")
-print(TGREEN +"=  ❌This Script Was Made By taj0023  and thelinuxuser-choice ❌        =")
-print(TGREEN +"=  ❌[youtube coming soon - coming soon with full of love]❌                 = ")
-print(TGREEN +"=        ❌[instergram - taj_0023,thelinuxuser_choice]❌                                   = ")
-print(TGREEN +"=============================================================================") 
-print(" -Here is your operating system-                          ")
 import os
-print(os.system('uname -a'))         
-print(TGREEN +"➊ SELECT NO 1 FOR PDF BRUTE FORCE ")
-print("                             ")
-print(TGREEN +"➋ SELECT NO 2 FOR ZIP BRUTE FORCE")
-print("                             ")
-print(TGREEN +"➌ SELECT NO 3 FOR HASH BRUTEFORCE")
-print("                             ")
-
-
 import subprocess as sub
 
-option = input("1,2 or 3:")
+option = input("1,2,3 or e:")
+if option == "e":
+ os.system('clear')           
+ print(bold +"────────────────────────░███")
+ print(bold +"────────────────────────█▒▒█")
+ print(bold +"─────────────███████████▒▒█")
+ print(bold +"──────────████░░░░░░░░█▒▒███")
+ print(bold +"────────███░░░████████▒▒█░░██")
+ print(bold +"───────██░░████▒▒▒▒▒█▒▒█▒██░██")
+ print(bold +"───────█░░██▒░░░░░██▒▒█▒▒▒▒█░██")
+ print(bold +"───────█░░░██░░░░█▒▒▒▒█░░░██░░█████")
+ print(bold +"───────█░░░░░████░████░███░░░░░░░░██")
+ print(bold +"───────█░░░░░░░░█████████░░░░░███░░█")
+ print(bold +"───────██░░░░░░░░░░░░░░░░░░░░█──██░█")
+ print(bold +"─────████░░░░░░░░▒▒░▒▒░░░░░░░████░░█")
+ print(bold +"───███▒▒██░░░░░░░▒▒▒▒▒░░░░░░░░░░░░█")
+ print(bold +"─███▒▒▒▒▒██░░░░░░░▒▒▒░░░░░░███████▒██")
+ print(bold +"─██▒▒▒▒▒▒▒██░░░░░░░▒░░░░░░██▒▒▒▒▒▒▒▒██")
+ print(bold +"██▒▒▒▒▒▒▒▒▒▒████░░░░░░░███▒▒▒▒▒▒▒▒▒▒▒██")
+ print(bold +"─██▒▒▒▒▒▒▒▒▒▒▒██████████▒▒▒▒▒▒▒▒▒▒▒▒▒██")
+ print(bold +"──███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██")
+ print(bold +"────███▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒████")
+ print(bold +"───────██████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████")
+ print(bold +"──────────█████████████████████ ")
+
+ print(TGREEN +"MAKE    ")
+ print(TGREEN +"   SURE")
+ print(TGREEN +"      TO")
+ print(TGREEN +"       HAVE")
+ print(TGREEN +"           A")
+ print(TGREEN +"           GOOD")
+ print(TGREEN +"             COFEE :)")
+    
+    
 if option == "1":
    filename = input(TGREEN +"ENTER YOUR FILE HERE=")
    wordlist = input(TGREEN +"ENTER YOUR WORDLIST HERE=")
@@ -59,7 +80,10 @@ else:
    sub.run("python3 hash.py".split())
 
 
-print(TGREEN +"THANK YOU!  ")
+print(WARNING +"THANK YOU!  ")
 print(TGREEN +"STAY TUNED FOR NEW HACKING TOOLS")
-print(TGREEN +"-------:GAME OVER:--------------")
+print(WARNING +"-------:GAME OVER:--------------")
+
+
+
 
